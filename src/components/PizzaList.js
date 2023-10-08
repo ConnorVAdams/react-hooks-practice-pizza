c106303e4e16d0ca86fb0d647f229e6045549221
Connor Adams <connorvadams@gmail.com>
2023-10-08 15:48:11 -0600

    Initial commit.

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList({ pizzas }) {
+
+  const pizzaList = pizzas.map(pizza => {
+    return <Pizza key={pizza.id} {...pizza}/>
+  })
+  
+
   return (
     <table className="table table-striped">
       <thead>
@@ -13,9 +19,7 @@ function PizzaList() {
         </tr>
       </thead>
       <tbody>
-        {
-          //render Pizza here
-        }
+        {pizzaList}
       </tbody>
     </table>
   );
